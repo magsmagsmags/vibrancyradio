5eee70adb17233bf5501e06c5d00982295878e92
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-04-10 14:44:15 -0500

    landing page nav, footer, and first content block

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+
+//     var spotifyFrameEl = $("#spotifyFrame");
+//     var spotifyDivEl = $("#spotifyDiv");
+//     var spotifyUrl = "https://open.spotify.com/embed/playlist/";
+
+
+//     console.log(spotifyUrl + userSign.aries);
+//     event.preventDefault();
+//     console.log("DEBUG: THIS WILL SHOW WHEN ARIES IS CLICKED");
+//     console.log("EXAMPLE OF BUTTON");
+//     // Adding name of Astrological Sign + Spotify Playlist to Title
+//     // $("#spotifyListName").text("Aries Spotify PlayList");
+//     // adding attribute of URL to iframe tag in index
+//     spotifyFrameEl.attr("src", spotifyUrl + userSign.aries);
+//     // appending the element to the div holding iframe
+//     spotifyDivEl.append(spotifyFrameEl);
+
+// });
